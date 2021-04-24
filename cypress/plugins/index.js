b74daa4e1afde4805c19b455215ad8f22524ce96
ElevateBart <ledouxb@gmail.com>
2021-04-24 11:01:33 -0500

    use modern buld of nuxt since we are using modern browsers

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -7,9 +7,7 @@ const { getWebpackConfig } = require("nuxt");
  */
 module.exports = (on, config) => {
   on("dev-server:start", async options => {
-    const webpackConfig = await getWebpackConfig("client", {
-      for: "dev"
-    });
+    const webpackConfig = await getWebpackConfig("modern", "dev");
 
     return startDevServer({
       options,
