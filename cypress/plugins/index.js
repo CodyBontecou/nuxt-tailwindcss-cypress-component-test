2416135a374299785ea56ba10e7693df126d47a9
ElevateBart <ledouxb@gmail.com>
2021-04-24 18:06:28 -0500

    add tailwind artificially to ct

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,13 +1,31 @@
 /// <reference types="cypress" />
 const { startDevServer } = require("@cypress/webpack-dev-server");
 const { getWebpackConfig } = require("nuxt");
+const tailwindPostCSSplugin = require("tailwindcss");
 
 /**
  * @type Cypress.PluginConfig
  */
 module.exports = (on, config) => {
   on("dev-server:start", async options => {
-    const webpackConfig = await getWebpackConfig("modern", "dev");
+    const webpackConfig = await getWebpackConfig("modern", {
+      for: "dev",
+      configOverides: {
+        tailwindcss: {
+          jit: false
+        }
+      }
+    });
+
+    webpackConfig.module.rules
+      // find the laoder for css
+      .find(r => r.test.toString().includes("css"))
+      // avoid touching anything with modules
+      .oneOf.find(l => !l.resourceQuery)
+      // find the postcss loader
+      .use.find(m => m.loader.includes("postcss"))
+      // add the tailwind plugin
+      .options.plugins.push(tailwindPostCSSplugin());
 
     return startDevServer({
       options,
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,7 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import "./commands";
-import "../../assets/css/tailwind.css";
+import "~/assets/css/tailwind.css";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
